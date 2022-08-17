96080fd79a63de7756f83028bf62da8a9cd0c7e1
elenaNikolaenya <rushka@list.ru>
2022-08-18 01:17:02 +0300

    Update script.js
    the task is done:
    - new elements can be added (except empty elements)
    -input is clean after that
    - elements can be crossed out (and uncrossed back)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
-
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
@@ -7,3 +6,28 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector('#input');
+const list = document.querySelector('.items');
+
+function makeListItem() {
+  const listItem = document.createElement('p');
+  const textListItem = input.value;
+
+  listItem.textContent = textListItem;
+  list.append(listItem);
+
+  input.value = '';
+  
+  listItem.addEventListener('click', function() {
+  listItem.classList.toggle('done');  
+})
+}
+
+input.addEventListener('keydown', function(event) {
+  if (event.key == 'Enter') {
+    if (input.value != '') {
+      makeListItem();
+    }    
+  }
+})
